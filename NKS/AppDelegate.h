27a98cdbde78aee0b868b9f434e1293b435c1dc1
Ryan Luce <ryan@ryanluce.com>
2012-07-02 14:49:05 -0400

    Initial Commit

---
+++ NKS/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NKS
+//
+//  Created by Ryan Luce on 7/2/12.
+//  Copyright (c) 2012 Ryan Luce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NKS/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NKS
+//
+//  Created by Ryan Luce on 7/2/12.
+//  Copyright (c) 2012 Ryan Luce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
