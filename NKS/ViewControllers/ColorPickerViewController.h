f533d24fc30f42161890156fb1f7d888e7242067
Ryan Luce <ryan@ryanluce.com>
2012-08-08 10:32:09 -0700

    Conversion to glkit

---
+++ NKS/ViewControllers/ColorPickerViewController.h
@@ -0,0 +1,13 @@
+//
+//  ColorPickerViewController.h
+//  NKS
+//
+//  Created by Ryan Luce on 7/16/12.
+//  Copyright (c) 2012 Ryan Luce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ColorPickerViewController : UIViewController
+
+@end
